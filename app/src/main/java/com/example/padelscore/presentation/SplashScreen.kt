9dbad0a08849fdb9cf644aa34b060a27e0bbbaa0
Loren Boyle <lboyle7@jaguarlandrover.com>
2026-05-09 17:39:41 +0100

    update splash screen design colours

--- app/src/main/java/com/example/padelscore/presentation/SplashScreen.kt
+++ app/src/main/java/com/example/padelscore/presentation/SplashScreen.kt
@@ -33,6 +33,7 @@ import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.StrokeJoin
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.wear.compose.material.Button
@@ -55,10 +56,11 @@ fun SplashScreen(onStartGame: (Int, Int) -> Unit) {
         verticalArrangement = Arrangement.Center
     ) {
         Text(
-            text = "Padel",
+            text = "PADEL",
             fontSize = TitleSize,
             fontWeight = WeightExtra,
-            color = OnSurface
+            color = CobaltLight,
+            textAlign = TextAlign.Center
         )
         Spacer(modifier = Modifier.height(4.dp))
         Text(
